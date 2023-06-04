9bc45bfab70d2f71962baf3ecd8d275992f58815
Fabian Ramirez <fabianramirez0072@gmail.com>
2023-06-04 00:01:59 -0500

    install react-native-paper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import StackNavigator from "../routes/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import { useColorScheme } from 'react-native';
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  PaperProvider,
+} from 'react-native-paper';
 
 const App = () => {
+  const colorScheme = useColorScheme();
+  const paperTheme =
+    colorScheme === 'dark'
+      ? { ...MD3DarkTheme}
+      : { ...MD3LightTheme};
+
+
   return (
-    <NavigationContainer>
-      <StackNavigator/>
-    </NavigationContainer>
+    <PaperProvider theme={paperTheme}>
+      <NavigationContainer>
+          <StackNavigator/>
+      </NavigationContainer>
+    </PaperProvider>
   )
 };
 
