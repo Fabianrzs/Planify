214a0e86a0133d556a0b1924c9619b19455279bb
Fabian Ramirez <fabianramirez0072@gmail.com>
2023-06-06 21:58:10 -0500

    modulations

---
+++ src/service/authService.tsx
@@ -0,0 +1,93 @@
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+
+GoogleSignin.configure({
+  webClientId:
+    '426388272963-809a7iqjd02qtabq2995npclhr9cvgeu.apps.googleusercontent.com',
+  //pasar a una variable de entorno
+});
+
+type userCredencial = Promise<FirebaseAuthTypes.UserCredential | undefined>;
+
+export default function () {
+  const {currentUser} = auth();
+
+  const singInAnonimous: () => userCredencial = async () => {
+    try {
+      console.log('sing in anonikous');
+      return await auth().signInAnonymously();
+    } catch (error: any) {
+      if (error.code === 'auth/operation-not-allowed') {
+        console.log('Enable anonymous in your firebase console.');
+      }
+      console.error(error);
+      return undefined;
+    }
+  };
+
+  const sigInEmailAndPassword: ({
+    email,
+    password,
+  }: any) => userCredencial = async ({email, password}: any) => {
+    try {
+      console.log('sigIn Email And Password');
+      return await auth().createUserWithEmailAndPassword(email, password);
+    } catch (error: any) {
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          console.log('That email address is already in use!');
+          break;
+        case 'auth/invalid-email':
+          console.log('That email address is invalid!');
+          break;
+        default:
+          console.error(error);
+          break;
+      }
+      return undefined;
+    }
+  };
+
+  const signInGoogle: () => userCredencial = async () => {
+    try {
+      console.log('sing in google');
+      await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+      const {idToken} = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      return await auth().signInWithCredential(googleCredential);
+    } catch (error: any) {
+      switch (error.code) {
+        case statusCodes.SIGN_IN_CANCELLED:
+          console.log('SIGN_IN_CANCELLED');
+          break;
+        case statusCodes.IN_PROGRESS:
+          console.log('IN_PROGRESS');
+          break;
+        case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+          console.log('PLAY_SERVICES_NOT_AVAILABLE');
+          break;
+        default:
+          console.log(error);
+          break;
+      }
+      return undefined;
+    }
+  };
+
+  const signOut = async () => {
+    console.log('Saliendo ');
+    await auth().signOut();
+  };
+
+  return {
+    singInAnonimous,
+    sigInEmailAndPassword,
+    signInGoogle,
+    signOut,
+    auth,
+    currentUser,
+  };
+}
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,18 +1,17 @@
 import {Text, TouchableOpacity, View} from 'react-native';
-import {
-  GoogleSignin,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import authService from '../service/authService';
 import {useEffect} from 'react';
 
-GoogleSignin.configure({
-  webClientId:
-    '426388272963-809a7iqjd02qtabq2995npclhr9cvgeu.apps.googleusercontent.com',
-});
-
 export default function () {
+  const {
+    singInAnonimous,
+    signOut,
+    signInGoogle,
+    sigInEmailAndPassword,
+    currentUser,
+  } = authService();
+
   const loadUsers = async () => {
     try {
       const querySnapshot = await firestore().collection('Users').get();
@@ -39,77 +38,11 @@ export default function () {
         console.log('User added!');
       });
   };
-  const singOut = async () => {
-    auth()
-      .signOut()
-      .then(() => console.log('User signed out!'));
-  };
-  const singInAnonimous = async () => {
-    auth()
-      .signInAnonymously()
-      .then(data => {
-        console.log('user', data);
-        console.log('User signed in anonymously');
-      })
-      .catch(error => {
-        if (error.code === 'auth/operation-not-allowed') {
-          console.log('Enable anonymous in your firebase console.');
-        }
-        console.error(error);
-      });
-  };
-  const sigInEmailAndPassword = async () => {
-    auth()
-      .createUserWithEmailAndPassword(
-        'jane.doe@example.com',
-        'SuperSecretPassword!',
-      )
-      .then(() => {
-        console.log('User account created & signed in!');
-      })
-      .catch(error => {
-        if (error.code === 'auth/email-already-in-use') {
-          console.log('That email address is already in use!');
-        }
-
-        if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
-        }
-
-        console.error(error);
-      });
-  };
-  const signInGoogle = async () => {
-    try {
-      await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-      // Get the users ID token
-      const {idToken} = await GoogleSignin.signIn();
-
-      // Create a Google credential with the token
-      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-      const usr = await auth().signInWithCredential(googleCredential);
-      console.log(usr);
-    } catch (error: any) {
-      console.log(error);
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log('SIGN_IN_CANCELLED');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('IN_PROGRESS');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('PLAY_SERVICES_NOT_AVAILABLE');
-      } else {
-        console.log('DEFAULT');
-      }
-    }
-  };
-
   useEffect(() => {
     checkSession().then();
   }, []);
 
   const checkSession = async () => {
-    const currentUser = auth().currentUser;
-
     if (currentUser) {
       // Extraer los datos del usuario
       const {uid, displayName, email, photoURL, phoneNumber} = currentUser;
@@ -120,69 +53,37 @@ export default function () {
       console.log('Correo electrónico:', email);
       console.log('Numero de telefono:', phoneNumber);
       console.log('URL de la foto de perfil:', photoURL);
-
-      const userRef = firestore().collection('Users').doc(uid);
-      const userSnapshot = await userRef.get();
-
-      if (userSnapshot.exists) {
-        // Extraer los datos adicionales del usuario
-        const userData = userSnapshot.data();
-        // Utilizar los datos según tus necesidades
-        console.log('Datos adicionales del usuario:', userData);
-      } else {
-        console.log('No se encontraron datos adicionales para el usuario');
-      }
     } else {
       console.log('No hay usuario autenticado');
     }
   };
-  /*const onFacebookButtonPress = async () => {
-    // Attempt login with permissions
-    const result = await LoginManager.logInWithPermissions([
-      'public_profile',
-      'email',
-    ]);
-
-    if (result.isCancelled) {
-      throw 'User cancelled the login process';
-    }
-
-    // Once signed in, get the users AccesToken
-    const data = await AccessToken.getCurrentAccessToken();
-
-    if (!data) {
-      throw 'Something went wrong obtaining access token';
-    }
-
-    // Create a Firebase credential with the AccessToken
-    const facebookCredential = auth.FacebookAuthProvider.credential(
-      data.accessToken,
-    );
-
-    // Sign-in the user with the credential
-    await auth().signInWithCredential(facebookCredential);
-  };*/
   return (
     <View>
-      <TouchableOpacity onPress={() => signInGoogle()}>
+      <TouchableOpacity onPress={signInGoogle}>
         <Text>LOGIN GOOGLE</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => singInAnonimous()}>
+      <TouchableOpacity onPress={singInAnonimous}>
         <Text>Anonimous</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => sigInEmailAndPassword()}>
+      <TouchableOpacity
+        onPress={() =>
+          sigInEmailAndPassword({
+            email: 'jane.doe@example.com',
+            password: 'SuperSecretPassword!',
+          }).then(data => console.log(data))
+        }>
         <Text>LOGIN USER AND PASSWORD</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => singOut()}>
+      <TouchableOpacity onPress={signOut}>
         <Text>SALIR</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => loadUsers()}>
+      <TouchableOpacity onPress={loadUsers}>
         <Text>LOAD USERS</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => saveUsers()}>
+      <TouchableOpacity onPress={saveUsers}>
         <Text>SAVE USER</Text>
       </TouchableOpacity>
     </View>
