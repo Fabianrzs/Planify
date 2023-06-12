87a69ffda101e6ec2803d4e7758ddfe06cb28e4c
Fabian Ramirez <fabianramirez0072@gmail.com>
2023-06-11 19:04:26 -0500

    agrate index

---
+++ src/hook/index.ts
@@ -0,0 +1,6 @@
+import useAsyncStore from 'hook/useAsyncStore';
+import useAuth from 'hook/useAuth';
+import useProyectStore from 'hook/useProyectStore';
+import useYupSchema from 'hook/useYupSchema';
+
+export {useYupSchema, useAsyncStore, useProyectStore, useAuth};
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import DrawerNavigator from 'routes/DrawerNavigator';
+import StackNavigator from 'routes/StackNavigator';
+import TabNavigator from 'routes/TabNavigator';
+
+export {DrawerNavigator, StackNavigator, TabNavigator};
